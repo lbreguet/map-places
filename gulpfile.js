d74ef79fd670ca92a6b90ca315677522d86ae83f
lbreguet <louis-w@live.com>
2017-06-03 02:57:44 -0400

    Map functionality
    -map is shown
    -search bar doesn't work
    -add files into gulp
    -switch css to scss
    -implement backbones

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,12 @@
 var gulp = require('gulp');
+var sass = require('gulp-sass');
 
-gulp.task('default', function () {
+gulp.task('styles', function() {
+  gulp.src('css/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./css/'));
+});
 
+gulp.task('default', function() {
+  gulp.watch('css/*.scss', ['styles']);
 });
