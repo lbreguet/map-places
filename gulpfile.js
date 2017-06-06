189456ae7912e170fc9b70f7482017ccb57b497a
lbreguet <louis-w@live.com>
2017-06-06 11:29:05 -0400

    Gulpfile parsing js and sass
    -concat scripts and styles
    -minified scripts and styles

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,28 @@
 var gulp = require('gulp');
-var sass = require('gulp-sass');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+var compass = require('gulp-compass');
 
-gulp.task('styles', function() {
-  gulp.src('css/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./css/'));
+gulp.task('scripts', function() {
+  gulp.src('assets/scripts/*.js')
+    .pipe(concat('all.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/scripts'));
 });
 
-gulp.task('default', function() {
-  gulp.watch('css/*.scss', ['styles']);
+gulp.task('compass', function() {
+  gulp.src('assets/styles/*.scss')
+    .pipe(compass({
+      config_file: './config.rb',
+      css: './assets/styles',
+      sass: './assets/styles'
+    }))
+    .pipe(gulp.dest('dist/styles'));
 });
+
+gulp.task('watch', function() {
+  gulp.watch('assets/scripts/*.js', ['scripts']);
+  gulp.watch('assets/styles/*.scss', ['compass']);
+});
+
+gulp.task('default', ['scripts', 'compass', 'watch']);
