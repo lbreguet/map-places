86658cf5ad65598690ac63311abc43724f6ea693
lbreguet <louis-w@live.com>
2017-06-06 13:11:32 -0400

    Refine sass
    -sass is organized
    -starting new sass for page visualization

--- assets/scripts/initAutocomplete.js
+++ assets/scripts/initAutocomplete.js
@@ -1,8 +1,8 @@
 function initAutocomplete() {
   var map = new google.maps.Map(document.getElementById('map'), {
     center: {
-      lat: -33.8688,
-      lng: 151.2195
+      lat: 40.7128,
+      lng: -74.0059
     },
     zoom: 13,
     mapTypeId: 'roadmap'
